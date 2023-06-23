09b89b23bcce1782e1e92fc65ba553e482411b42
Rao_Sahab <101115115+Arin-KA@users.noreply.github.com>
2023-06-23 20:59:52 +0530

    Update script.js

--- back_end/script.js
+++ back_end/script.js
@@ -8,27 +8,54 @@ app.get("/", function(req, res){
     
     
 });
-app.post("/", function(req, res){
+app.post("/", function(req, res) {
+  const query = req.body.cityName;
+  const apiKey = "89605f9e9aaa7edd3b1908b22624f45b";
+  const unit = "metric";
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&units=" + unit + "&appid=" + apiKey;
+  
+  https.get(url, function(response) {
+    let data = "";
     
-    const query=req.body.cityName;
-    const apiKey="89605f9e9aaa7edd3b1908b22624f45b";
-    const unit="metric";
-    const url="https://api.openweathermap.org/data/2.5/weather?q="+ query + "&units=metric&appid=89605f9e9aaa7edd3b1908b22624f45b" ;
-    https.get(url, function(response){
-        console.log(response.statusCode);
-        response.on("data", function(data){
-            const wd=JSON.parse(data);
-            const temp=wd.main.temp;
-            const desc=wd.weather[0].description;
-            const icon=wd.weather[0].icon;
-            const imgURL="http://openweathermap.org/img/wn/"+icon+"@2x.png";
-            res.write("<p>The weather is currently"+desc+"</p>");
-            res.write("<h1>"+temp+"</h1>");
-            res.write("<img src="+imgURL+ ">");
-            res.send();
-        })
-    })
-})
+    response.on("data", function(chunk) {
+      data += chunk;
+    });
+
+    response.on("end", function() {
+      const wd = JSON.parse(data);
+      const temp = wd.main.temp;
+      const desc = wd.weather[0].description;
+      const icon = wd.weather[0].icon;
+      const imgURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+      
+      res.write("<p>The weather is currently " + desc + "</p>");
+      res.write("<h1>" + temp + "</h1>");
+      res.write("<img src=" + imgURL + ">");
+      res.send();
+    });
+  });
+});
+// app.post("/", function(req, res){
+    
+//     const query=req.body.cityName;
+//     const apiKey="89605f9e9aaa7edd3b1908b22624f45b";
+//     const unit="metric";
+//     const url="https://api.openweathermap.org/data/2.5/weather?q="+ query + "&units=metric&appid=89605f9e9aaa7edd3b1908b22624f45b" ;
+//     https.get(url, function(response){
+//         console.log(response.statusCode);
+//         response.on("data", function(data){
+//             const wd=JSON.parse(data);
+//             const temp=wd.main.temp;
+//             const desc=wd.weather[0].description;
+//             const icon=wd.weather[0].icon;
+//             const imgURL="http://openweathermap.org/img/wn/"+icon+"@2x.png";
+//             res.write("<p>The weather is currently"+desc+"</p>");
+//             res.write("<h1>"+temp+"</h1>");
+//             res.write("<img src="+imgURL+ ">");
+//             res.send();
+//         })
+//     })
+// })
 
 app.listen(3500, function(){
     console.log("server is running on port 3500.");
